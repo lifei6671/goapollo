12e96c7bd6efa499537c55a8089b915fac5d168c
lifei6671 <longfei6671@163.com>
2019-02-22 16:56:35 +0800

    feat:实现文件备份

--- serialization.go
+++ serialization.go
@@ -17,6 +17,27 @@ type Serialization struct {
 	Notification   []*ApolloConfigNotification `json:"notification"`
 }
 
+// NewSerializationWithFile 从文件序列化配置信息
+func NewSerializationWithFile(filename string) (s *Serialization, err error) {
+
+	if f, err := os.Open(filename); err != nil {
+		return nil, err
+	} else {
+		var obj Serialization
+		err = gob.NewDecoder(f).Decode(&obj)
+		return &obj, err
+	}
+}
+func (s *Serialization) Save(filename string) error {
+	_ = os.MkdirAll(filepath.Dir(filename), 0755)
+
+	if f, err := os.Create(filename); err != nil {
+		return err
+	} else {
+		return gob.NewEncoder(f).Encode(s)
+	}
+}
+
 func (s *Serialization) String() string {
 	b, _ := json.Marshal(s)
 
@@ -54,5 +75,6 @@ func (s *SerializationObject) Save(filename string) error {
 }
 
 func init() {
+	gob.Register(Serialization{})
 	gob.Register(SerializationObject{})
 }
